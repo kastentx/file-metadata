c7407c403b53f324d5f678fb7ca8cd5994473d19
Nick Kasten <kastentx@gmail.com>
2016-06-25 07:30:39 +0000

    third commit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require( 'express' )
 var path = require( 'path' )
+var multer = require( 'multer' )
+var upload = multer( )
 var app = express( )
 
 app.use( express.static( path.resolve( __dirname ) ) )
@@ -8,6 +10,13 @@ app.get( '/', function( req, res ) {
   res.render( 'index.html' )
 } )
 
+// handle file upload
+app.post( '/upload', upload.single('targetFile'),  function ( req, res ) {
+    
+    // do something with image
+    res.json( { 'size' : req.file.size } )
+} )
+
 app.listen( process.env.PORT, function( port ) {
-  console.log( "Listening at " + process.env.PORT )
+  console.log( 'Listening at ' + process.env.PORT )
 } )
